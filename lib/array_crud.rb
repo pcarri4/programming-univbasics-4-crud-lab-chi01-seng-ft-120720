503dac6f43edc84e5123a0a6b38d7b1cd96c085e
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-24 07:11:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  my_array = ["blue", "yellow", "green", "blue"]
+  my_array = ["blue", "yellow", "green", "arrays!"]
 
   blue_my_array = my_array.pop
 end
