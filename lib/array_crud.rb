fe6d9cc7fd94b537e70143ab3eb4919019c8757f
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-24 06:51:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array(colors)
-  colors = ["red", "blue", "green", "yellow"]
+  colors = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
