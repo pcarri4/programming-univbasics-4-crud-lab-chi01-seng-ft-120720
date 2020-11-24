4f28cb527b65e4296ecae580aa9a222c3ff0131c
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-24 07:15:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ end
 def remove_element_from_start_of_array(array)
   my_array = ["blue", "yellow", "green", "arrays!"]
   
-  blue = my_array.shift
+  wow = my_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
