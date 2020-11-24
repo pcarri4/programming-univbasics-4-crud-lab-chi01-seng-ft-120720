3100ab9eb19aa681472290b6f93640752bc78855
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-24 07:03:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,9 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
+  my_array = [1, 2, 3, 4]
   
+  my_array.unshift(arrays!)
 end
 
 def remove_element_from_end_of_array(array)
